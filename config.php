8bc17ebc8795be1c90ec95ae981223257ddeb138
chheng <chheng@gmail.com>
2025-02-26 23:17:12 +0700

    add the .env.example and add composer

--- config.php
+++ config.php
@@ -2,9 +2,11 @@
 
 return [
     'database' => [
-        'host' => 'localhost',
-        'port' => 3306,
-        'dbname' => 'rpshopdot',
-        'charset' => 'utf8mb4'
-    ],
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+        ],
 ];
