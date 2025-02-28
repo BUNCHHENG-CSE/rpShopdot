8230641a5b2f6adea01b617796d73f1fee4a6c57
ChartNoir <keosivphanchart123@gmail.com>
2025-03-01 02:11:08 +0700

    hello productcontroller v2

--- Http/controller/client/products/index.php
+++ Http/controller/client/products/index.php
@@ -1,16 +1,3 @@
 <?php
 
-use Core\Database;
-use Core\ProductController;
-$config = require base_path('config.php')['database'];
-
-$database = new Database($config);
-
-$productController = new ProductController($database);
-$products = $productController->index();
-
-dd(afdsas);
-view('dashboard/products/index.view.php', [
-    'heading' => 'Products Management',
-    'products' => $products
-]);
+view("client/products/index.view.php");
--- routes.php
+++ routes.php
@@ -16,7 +16,12 @@ $router->get('/dashboard', 'dashboard/index.php');
 $router->get('/table', 'dashboard/table.php');
 $router->get('/billing', 'dashboard/billing.php');
 $router->get('/profile', 'dashboard/profile.php');
-$router->get('/tbproducts', 'dashboard/products/index.php');
+// $router->get('/tbproducts', 'dashboard/products/index.php');
 $router->get('/tbcategories', 'dashboard/categories/index.php');
 $router->get('/tbusers', 'dashboard/users/index.php');
 $router->get('/tborders', 'dashboard/orders/index.php');
+//product
+$router->get('/tbproducts', 'dashboard/products/index.php');
+$router->post('/tbproducts', 'dashboard/products/index.php');
+$router->post('/tbproducts/{id}', 'dashboard/products/index.php');
+$router->post('/tbproducts/{id}/delete', 'dashboard/products/index.php');
